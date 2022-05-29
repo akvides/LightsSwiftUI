e0fb2081ca0f5f5404300c264bb780d05d3eac89
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-05-29 17:17:11 +0300

    Initial Commit

---
+++ LightsSwiftUI/LightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsSwiftUIApp.swift
+//  LightsSwiftUI
+//
+//  Created by Василий Полторак on 29.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
